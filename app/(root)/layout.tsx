816b2d1272c79a635ee6729ff8dc2f387ee6ac30
Ber <bsianturiandrean@gmail.com>
2025-01-09 02:03:28 +0700

    feat: add footer to layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,18 @@
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
 export default async function RootLayout({
-    children,
-} : {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <>
-        <main className="w-full">
-            <Navbar />
-            {children}
-        </main>
-        </>
-    )
+  return (
+    <>
+      <main className="w-full">
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+    </>
+  );
 }
