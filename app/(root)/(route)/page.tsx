f8b4211e4abc1e239430e90846ae5091d45a698b
Ber <bsianturiandrean@gmail.com>
2025-01-09 02:03:17 +0700

    feat: add sections to homepage

--- app/(root)/(route)/page.tsx
+++ app/(root)/(route)/page.tsx
@@ -1,7 +1,13 @@
+import BMISection from "./components/bmi";
+import HeroSection from "./components/hero";
+import ServicesSection from "./components/services";
+
 export default function Root() {
   return (
-    <div>
-      This is home
+    <div className="w-full flex flex-col gap-x-4 bg-nc-bg">
+      <HeroSection />
+      <BMISection />
+      <ServicesSection />
     </div>
   );
 }
