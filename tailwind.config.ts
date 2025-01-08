f9ffecd03a22e1b0dacbe51e823bfe41e9d86596
Ber <bsianturiandrean@gmail.com>
2025-01-08 22:59:58 +0700

    update config and package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,79 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        nc: {
+          green: {
+            DEFAULT: "hsl(var(--nc-green))",
+            hover: "hsl(var(--nc-green-hover))",
+            light: "hsl(var(--nc-light-green))",
+          },
+          orange: {
+            DEFAULT: "hsl(var(--nc-orange))",
+            hover: "hsl(var(--nc-orange-hover))",
+            light: "hsl(var(--nc-light-orange))",
+          },
+          white: "hsl(var(--nc-white))",
+          secondary: "hsl(var(--nc-secondary))",
+          black: "hsl(var(--nc-black))",
+          bg: "hsl(var(--nc-bg))",
+        },
+        bmi: {
+          underweight: "hsl(var(--nc-bmi-underweight))",
+          normal: "hsl(var(--nc-bmi-normal))",
+          overweight: "hsl(var(--nc-bmi-overweight))",
+          obese: {
+            one: "hsl(var(--nc-bmi-1))",
+            two: "hsl(var(--nc-bmi-2))",
+            three: "hsl(var(--nc-bmi-3))",
+          },
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
