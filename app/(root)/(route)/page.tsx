34b44a36b29679d5fa01a180428077b52f45a509
Ber <bsianturiandrean@gmail.com>
2025-01-09 20:57:03 +0700

    style: fix styling in homepage

--- app/(root)/(route)/page.tsx
+++ app/(root)/(route)/page.tsx
@@ -4,7 +4,7 @@ import ServicesSection from "./components/services";
 
 export default function Root() {
   return (
-    <div className="w-full flex flex-col gap-x-4 bg-nc-bg">
+    <div className="w-full flex flex-col gap-y-4 bg-nc-bg pb-10">
       <HeroSection />
       <BMISection />
       <ServicesSection />
