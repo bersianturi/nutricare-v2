048fb6f08b606f4a2e0e833f879ae9dffb4ed117
Ber <bsianturiandrean@gmail.com>
2025-01-09 20:58:41 +0700

    feat: add auth form modal

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,9 +11,16 @@ import {
   navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
 import { Button } from "./ui/button";
+import SignInModal from "./modals/sign-in-modal";
+import { useState } from "react";
+import SignUpModal from "./modals/sign-up-modal";
 
 const Navbar = () => {
   const pathname = usePathname();
+
+  const [signInOpen, setSignInOpen] = useState(false);
+  const [signUpOpen, setSignUpOpen] = useState(false);
+
   const menuItems = [
     {
       title: "Home",
@@ -43,57 +50,75 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="border-b sticky top-0 z-50 bg-white">
-      <div className="flex h-16 items-center justify-between lg:px-[100px] px-[25px]">
-        <div className="flex items-center mt-[-6px]">
-          <Link href={"/"}>
-            <Image
-              src={"/Logo.png"}
-              alt="nutricare Logo"
-              width={120}
-              height={100}
-            />
-          </Link>
+    <>
+      <SignInModal
+        isOpen={signInOpen}
+        onClose={() => setSignInOpen(false)}
+        onSignUp={() => {
+          setSignInOpen(false);
+          setSignUpOpen(true);
+        }}
+      />
+      <SignUpModal
+        isOpen={signUpOpen}
+        onClose={() => setSignUpOpen(false)}
+        onSignIn={() => {
+          setSignUpOpen(false);
+          setSignInOpen(true);
+        }}
+      />
+      <nav className="border-b sticky top-0 z-50 bg-white">
+        <div className="flex h-16 items-center justify-between lg:px-[100px] px-[25px]">
+          <div className="flex items-center mt-[-6px]">
+            <Link href={"/"}>
+              <Image
+                src={"/Logo.png"}
+                alt="nutricare Logo"
+                width={120}
+                height={100}
+              />
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <NavigationMenu>
+              <NavigationMenuList>
+                {menuItems.map((item) => (
+                  <NavigationMenuItem key={item.title}>
+                    <Link href={item.url} legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={
+                          navigationMenuTriggerStyle() +
+                          " text-nc-secondary bg-transparent hover:bg-transparent hover:text-nc-green focus:bg-transparent focus:text-nc-green active:text-nc-green"
+                        }
+                      >
+                        {item.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <Button
+              size={"default"}
+              variant={"orange"}
+              className="rounded-full"
+              onClick={() => setSignInOpen(true)}
+            >
+              Login
+            </Button>
+            <Button
+              variant={"link"}
+              className="hover:text-nc-green hover:no-underline text-nc-secondary"
+              onClick={() => setSignUpOpen(true)}
+            >
+              Register
+            </Button>
+          </div>
         </div>
-        <div className="flex items-center">
-          <NavigationMenu>
-            <NavigationMenuList>
-              {menuItems.map((item) => (
-                <NavigationMenuItem key={item.title}>
-                  <Link href={item.url} legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={
-                        navigationMenuTriggerStyle() +
-                        " text-nc-secondary bg-transparent hover:bg-transparent hover:text-nc-green focus:bg-transparent focus:text-nc-green active:text-nc-green"
-                      }
-                    >
-                      {item.title}
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              ))}
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <Button
-            size={"default"}
-            variant={"orange"}
-            asChild
-            className="rounded-full"
-          >
-            <Link href={"/login"}>Login</Link>
-          </Button>
-          <Button
-            variant={"link"}
-            asChild
-            className="hover:text-nc-green hover:no-underline text-nc-secondary"
-          >
-            <Link href={"/register"}>Register</Link>
-          </Button>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
