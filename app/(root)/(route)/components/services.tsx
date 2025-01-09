e2302e37af01fe0e418554eab00d43850771c801
Ber <bsianturiandrean@gmail.com>
2025-01-09 20:57:41 +0700

    feat: update service section component

--- app/(root)/(route)/components/services.tsx
+++ app/(root)/(route)/components/services.tsx
@@ -1,11 +1,49 @@
-import Image from "next/image";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  CookingPot,
+  Grid2x2Check,
+  MessagesSquare,
+  Newspaper,
+} from "lucide-react";
 
 const ServicesSection = () => {
+  const services = [
+    {
+      title: "BMI Calculate",
+      description:
+        "Calculate your Body Mass Index (BMI) effortlessly and receive insights into your weight and overall health status. Take the first step toward a healthier you by knowing your BMI.",
+      icon: Grid2x2Check,
+    },
+    {
+      title: "Article",
+      description:
+        "Our articles cover a wide range of topics to empower you with knowledge and tips for making positive choices in your daily life. Stay informed and inspired on your wellness journey.",
+      icon: Newspaper,
+    },
+    {
+      title: "Community",
+      description:
+        "Discuss your experiences, seek advice, and provide support to others. Together, we create a supportive space for achieving better health.",
+      icon: MessagesSquare,
+    },
+    {
+      title: "Food Nutrient",
+      description:
+        "Discover the nutrient content of various foods to make informed dietary choices. Whether you're tracking calories, macronutrients, or specific vitamins.",
+      icon: CookingPot,
+    },
+  ];
+
   return (
     <section id="service">
       <div className="flex justify-center items-center lg:py-[50px] py-[25px] px-[25px] lg:px-[150px]">
-        <div className="lg:max-w-[1080px] flex flex-col justify-center items-center gap-x-10">
-          <div className="flex flex-col mb-8">
+        <div className="lg:max-w-[1080px] flex flex-col justify-center items-center gap-x-10 gap-y-12">
+          <div className="flex flex-col">
             <h1 className="text-3xl font-bold lg:text-4xl text-center">
               <span className="text-nc-green">Our</span>{" "}
               <span className="text-nc-orange">Services</span>
@@ -14,77 +52,25 @@ const ServicesSection = () => {
               NutriCare provides several services. Below is the details.
             </p>
           </div>
-          <div className="grid grid-cols-2 gap-24">
-            <div className="flex flex-col gap-y-2">
-              <Image
-                src="/icon/calculator.svg"
-                alt="service"
-                width={30}
-                height={30}
-              />
-              <div className="flex flex-col">
-                <h4 className="text-xl font-semibold text-nc-green">
-                  BMI Calculate
-                </h4>
-                <p className="font-medium">
-                  Calculate your Body Mass Index (BMI) effortlessly and receive
-                  insights into your weight and overall health status. Take the
-                  first step toward a healthier you by knowing your BMI.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Image
-                src="/icon/article.svg"
-                alt="service"
-                width={40}
-                height={40}
-              />
-              <div className="flex flex-col">
-                <h4 className="text-xl font-semibold text-nc-green">Article</h4>
-                <p className="font-medium">
-                  Calculate your Body Mass Index (BMI) effortlessly and receive
-                  insights into your weight and overall health status. Take the
-                  first step toward a healthier you by knowing your BMI.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Image
-                src="/icon/community.svg"
-                alt="service"
-                width={40}
-                height={40}
-              />
-              <div className="flex flex-col">
-                <h4 className="text-xl font-semibold text-nc-green">
-                  Community
-                </h4>
-                <p className="font-medium">
-                  Calculate your Body Mass Index (BMI) effortlessly and receive
-                  insights into your weight and overall health status. Take the
-                  first step toward a healthier you by knowing your BMI.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <Image
-                src="/icon/food.svg"
-                alt="service"
-                width={40}
-                height={40}
-              />
-              <div className="flex flex-col">
-                <h4 className="text-xl font-semibold text-nc-green">
-                  Food Nutrient
-                </h4>
-                <p className="font-medium">
-                  Calculate your Body Mass Index (BMI) effortlessly and receive
-                  insights into your weight and overall health status. Take the
-                  first step toward a healthier you by knowing your BMI.
-                </p>
-              </div>
-            </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            {services.map((service) => (
+              <Card
+                key={service.title}
+                className="hover:-translate-y-1 hover:shadow-lg transition duration-300 ease-in-out"
+              >
+                <CardHeader className="gap-y-1">
+                  <div className="flex justify-start items-center">
+                    <div className="p-2 bg-nc-orange rounded-md shadow-sm">
+                      <service.icon color="white" size={30} />
+                    </div>
+                  </div>
+                  <CardTitle className="text-nc-green text-lg">
+                    {service.title}
+                  </CardTitle>
+                  <CardDescription>{service.description}</CardDescription>
+                </CardHeader>
+              </Card>
+            ))}
           </div>
         </div>
       </div>
