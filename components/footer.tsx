262f981be6f04fc81e65a7a8d6c45a139bf9c08b
Ber <bsianturiandrean@gmail.com>
2025-01-09 20:58:07 +0700

    style: add responsive styling to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
 
 const Footer = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex justify-between items-center w-full py-10 max-w-[1080px]">
-        <div className="flex flex-col max-w-[400px]">
+      <div className="flex flex-col lg:flex-row gap-y-4 px-8 lg:px-0 justify-between items-start lg:items-center w-full py-10 max-w-[1080px]">
+        <div className="flex flex-col max-w-[400px] gap-y-2">
           <Image
             src={"/Logo.png"}
             alt="nutricare Logo"
@@ -18,7 +24,43 @@ const Footer = () => {
             diet, or learning about food nutrition, we’ve got you covered.
           </p>
         </div>
-        <div className="flex flex-col gap-y-2">
+        <Accordion type="single" collapsible className="lg:hidden w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="hover:no-underline">
+              <h4 className="font-semibold text-nc-green text-xl hover:no">
+                Quick <span className="text-nc-orange">Links</span>
+              </h4>
+            </AccordionTrigger>
+            <AccordionContent>
+              <Link href={"#"}>Home</Link>
+            </AccordionContent>
+            <AccordionContent>
+              <Link href={"#"}>Article</Link>
+            </AccordionContent>
+            <AccordionContent>
+              <Link href={"#"}>About Us</Link>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" collapsible className="lg:hidden w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="hover:no-underline">
+              <h4 className="font-semibold text-nc-green text-xl hover:no">
+                Services
+              </h4>
+            </AccordionTrigger>
+            <AccordionContent>
+              <Link href={"#"}>BMI Calculate</Link>
+            </AccordionContent>
+            <AccordionContent>
+              <Link href={"#"}>Community</Link>
+            </AccordionContent>
+            <AccordionContent>
+              <Link href={"#"}>Food Nutrient</Link>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <div className="flex-col gap-y-2 hidden lg:flex">
           <h4 className="font-semibold text-nc-green text-xl">
             Quick <span className="text-nc-orange">Links</span>
           </h4>
@@ -32,7 +74,7 @@ const Footer = () => {
             About Us
           </Link>
         </div>
-        <div className="flex flex-col gap-y-2">
+        <div className="hidden lg:flex flex-col gap-y-2">
           <h4 className="font-semibold text-nc-green text-xl">Services</h4>
           <Link href={"#"} className="hover:text-nc-green">
             BMI Calculate
@@ -45,9 +87,9 @@ const Footer = () => {
           </Link>
         </div>
       </div>
-      <div className="flex py-4 justify-center items-center w-full border-t gap-1">
-        <p className="font-medium">&copy; 2024</p>
+      <div className="flex-col lg:flex-row flex py-6 lg:py-4 justify-center items-center w-full border-t gap-x-1">
         <div className="flex items-center">
+          <p className="font-medium me-1">&copy; 2024</p>
           <div className="mt-[-10px]">
             <Image
               src={"/logo-small.png"}
@@ -56,11 +98,11 @@ const Footer = () => {
               height={12}
             />
           </div>
-          <p className="font-medium">.</p>
+          <p className="font-medium">. All Rights Reserved.</p>
         </div>
-        <p>
-          All Rights Reserverd. |{" "}
-          <span className="font-bold">Dicoding Capstone Project 2023</span>
+        <p className="text-center font-bold">
+          <span className="hidden lg:inline"> | </span>Dicoding Capstone Project
+          2023
         </p>
       </div>
     </div>
